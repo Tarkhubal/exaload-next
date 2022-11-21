70c688f4cf82b5d5bed12e59ecdeee193ae2bb08
The_WildThomas <thb5309@gmail.com>
2022-11-21 07:03:05 +0100

    Nouveaux éléments sur la page home

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Link } from "next/link";
+import Link from "next/link";
 
 import '../styles/globals.css'
 import '../styles/footer.css'
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import Script from "next/script";
+import Link from "next/link";
+
 export default function Home() {
   return (
-      <>
-          <p>Le site Exaload</p>
-      </>
+    <>
+      <section>
+        <div id="container">
+          <span id="text1"></span>
+          <span id="text2"></span>
+        </div>
+        <svg id="filters">
+          <defs>
+            <filter id="threshold">
+              <feColorMatrix in="SourceGraphic" type="matrix"
+                values="1 0 0 0 0
+									                0 1 0 0 0
+								                  0 0 1 0 0
+									                0 0 0 255 -140"/>
+            </filter>
+          </defs>
+        </svg>
+      </section>
+      <Link href="/pricing">Test</Link>
+
+      <Script src="/js/anim-text-flow.js"></Script>
+    </>
   )
 }
